f3aa15814f3fec49999567a2e1c9c227bf0514fb
r4njith-kali <e0958148@u.nus.edu>
2025-04-21 14:46:35 +0800

    Test #1

--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Mini Router Web App</h1>
+      <Routes>
+        <Route path = '/' element = {<Home />} />
+        <Route path = '/about' element = {<About />} />
+      </Routes>
     </div>
   );
 }
