5b263fbedb7051d78ed673ace53c892a5ef6f348
r4njith-kali <e0958148@u.nus.edu>
2025-04-22 09:09:27 +0800

    Test #5

--- myfrontend/src/pages/Home.js
+++ myfrontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.css'; // Don't forget to import the CSS!
 
 function Home() {
     const navigate = useNavigate();
@@ -7,10 +8,11 @@ function Home() {
     return (
         <div>
             <h2>Welcome Page</h2>
-            <button onClick={() => navigate('/about')}>Go to About Page</button>
-            <button onClick={() => navigate('/contact')}>Contacts Page</button>
+            <div className = "home-container">
+                <button className="big-button" onClick={() => navigate('/about')}>Go to About Page</button>
+                <button onClick={() => navigate('/contact')}>Contacts Page</button>
+            </div>
         </div>
-
     );
 }
 
